70054a8e5e8b9e2b345c11ecb3340251368542c3
lynne <kimahlynne99@gmail.com>
2021-11-10 17:59:38 +0900

    시작 및 메인 화면 만듦. 애니메이션 부분 수정 필요

---
+++ app/src/main/java/com/example/psyche/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.psyche;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+//달 있는 곳
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        Button start_button = (Button) findViewById(R.id.start_button);
+        start_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), DiaryActivity.class);
+                startActivity(intent);
+                overridePendingTransition(0, 0);
+            }
+        });
+    }
+}
